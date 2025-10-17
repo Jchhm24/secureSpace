358489883613dc6ebe7975e665da81324d664875
Jchhm24 <jcchm2004@gmail.com>
2025-10-17 13:15:22 -0600

    se agregó el componente ButtonIcon con su plantilla y estilos, permitiendo la selección de diferentes tipos de botones con íconos

---
+++ src/app/shared/components/button-icon/button-icon.ts
@@ -0,0 +1,25 @@
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { History, LucideAngularModule, QrCode, SquarePen, Trash2, UserRoundPlus } from 'lucide-angular';
+
+@Component({
+  selector: 'app-button-icon',
+  imports: [LucideAngularModule, NgClass],
+  templateUrl: './button-icon.html',
+  styleUrl: './button-icon.css'
+})
+export class ButtonIcon {
+
+  type_button = input.required<
+    'add_user' | 'history' | 'qr_code' | 'edit' | 'delete'
+  >();
+  function_action = input.required<() => void>();
+
+  icons = {
+    personAdd: UserRoundPlus,
+    history: History,
+    qrCode: QrCode,
+    edit: SquarePen,
+    trash: Trash2,
+  }
+}
