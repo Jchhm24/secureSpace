8e2423d5a45c101624ab2ed82dc4426dc28ecd0b
Jchhm24 <jcchm2004@gmail.com>
2025-10-17 00:02:51 -0600

    se definieron las rutas de la aplicación para las páginas de inicio de sesión y bodegas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Login - SecureSpace',
+    loadComponent: () => import('./features/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'warehouses',
+    title: 'Bodegas - SecureSpace',
+    loadComponent: () =>
+      import('./features/warehouses/warehouses').then((m) => m.Warehouses),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
