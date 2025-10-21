936e3abcf78c23614c065e6a4267e89b9e3b5b27
Jchhm24 <jcchm2004@gmail.com>
2025-10-21 09:03:54 -0600

    se agrego el service de los warehouses

---
+++ src/app/core/services/warehouse-service.ts
@@ -0,0 +1,33 @@
+import { Injectable, signal } from '@angular/core';
+import { warehouses } from '@features/warehouses/const/warehouses-data';
+import { Warehouse } from '@features/warehouses/interfaces/warehouse-interface';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WarehouseService {
+  state = signal({
+    warehouses: new Map<number, Warehouse>(),
+  });
+
+  constructor() {
+    this.getWarehouses();
+  }
+
+  getWarehouses() {
+    const mockWarehouses: Warehouse[] = warehouses;
+
+    of(mockWarehouses).subscribe((result) => {
+      result.forEach((warehouse) => {
+        this.state().warehouses.set(warehouse.id, warehouse);
+      });
+
+      this.state.set({ warehouses: this.state().warehouses });
+    });
+  }
+
+  getFormattedWarehouses() {
+    return Array.from(this.state().warehouses.values());
+  }
+}
