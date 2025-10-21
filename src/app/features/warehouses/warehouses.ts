604076eaf5064b39d919696ada12e584d26ae67d
Jchhm24 <jcchm2004@gmail.com>
2025-10-21 09:01:34 -0600

    se componetiso mas la sección de los warehouses

--- src/app/features/warehouses/warehouses.ts
+++ src/app/features/warehouses/warehouses.ts
@@ -1,28 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { LayoutService } from '@core/services/layout-service';
-import { ButtonIcon } from '@shared/components/button-icon/button-icon';
-import { InputComponent } from '@shared/components/input-component/input-component';
 import { WarehouseCard } from '@shared/components/warehouse-card/warehouse-card';
-import { LucideAngularModule, ChartBar, Search } from 'lucide-angular';
-import { BadgeEnable } from './components/badge-enable/badge-enable';
+import { WarehousesTable } from './components/warehouses-table/warehouses-table';
 
 @Component({
   selector: 'app-warehouses',
-  imports: [
-    WarehouseCard,
-    InputComponent,
-    ButtonIcon,
-    LucideAngularModule,
-    BadgeEnable,
-  ],
+  imports: [WarehouseCard, WarehousesTable],
   templateUrl: './warehouses.html',
   styleUrl: './warehouses.css',
 })
 export class Warehouses implements OnInit {
   private layoutService = inject(LayoutService);
-
-  searchControl = new FormControl<string>('');
 
   ngOnInit(): void {
     this.layoutService.setConfig({
@@ -36,14 +24,5 @@ export class Warehouses implements OnInit {
         },
       },
     });
-  }
-
-  icons = {
-    chartBar: ChartBar,
-    search: Search,
-  };
-
-  functionButton() {
-    alert('solo para representar el boton');
   }
 }
