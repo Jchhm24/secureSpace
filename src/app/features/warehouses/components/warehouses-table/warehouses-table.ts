c5106aa582a2aa08d0716c11dc3921eec3b7d926
Jchhm24 <jcchm2004@gmail.com>
2025-10-21 08:58:50 -0600

    se creo el componente de tabla para los warehouses

---
+++ src/app/features/warehouses/components/warehouses-table/warehouses-table.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { InputComponent } from '@shared/components/input-component/input-component';
+import { LucideAngularModule, ChartBar, Search } from 'lucide-angular';
+import { BadgeEnable } from '../badge-enable/badge-enable';
+import { ButtonIcon } from '@shared/components/button-icon/button-icon';
+import { WarehouseService } from '@core/services/warehouse-service';
+import { Warehouse } from '@features/warehouses/interfaces/warehouse-interface';
+import { paginateTable } from '@shared/pipes/paginateTable';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-warehouses-table',
+  imports: [LucideAngularModule, InputComponent, BadgeEnable, ButtonIcon, NgClass],
+  templateUrl: './warehouses-table.html',
+  styleUrl: './warehouses-table.css',
+})
+export class WarehousesTable implements OnInit {
+
+  private readonly ITEMS_PER_PAGE = 5;
+
+  icons = {
+    chartBar: ChartBar,
+    search: Search,
+  };
+
+  searchControl = new FormControl('');
+  warehouseService = inject(WarehouseService);
+  warehouses = signal<Warehouse[]>([]);
+  groupedWarehouses = signal<Warehouse[][]>([]);
+  index_page = signal(0);
+
+  ngOnInit(): void {
+    this.getPaginatedData(this.ITEMS_PER_PAGE, this.warehouseService.getFormattedWarehouses());
+  }
+
+  getPaginatedData(limit: number, data: Warehouse[] | undefined): void {
+    const tableIndications = paginateTable(limit, data);
+    this.groupedWarehouses.set(tableIndications.groupedData);
+    this.warehouses.set(tableIndications.paginatedData);
+  }
+
+  changePage(page: number) : void {
+    this.index_page.set(page);
+    this.warehouses.set(this.groupedWarehouses()[page] || []);
+  }
+
+  functionButton() {
+    alert('solo para representar el boton');
+  }
+}
