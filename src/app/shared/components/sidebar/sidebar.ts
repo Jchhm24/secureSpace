d878d0e0d26cafbf95c726df887e93b6bed20372
Jchhm24 <jcchm2004@gmail.com>
2025-10-21 18:53:02 -0600

    se agrego estructura y estilos al sidebar, al igual que logica para que cambien los estilos denpendiendo del link

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { NgClass, NgOptimizedImage } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { LogOut, LucideAngularModule, MapPin, UserRound, Warehouse } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NgOptimizedImage, RouterLink, LucideAngularModule, NgClass],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  currentRoute = computed(() => this.route.snapshot.url[0].path);
+
+  logOutIcon = LogOut;
+
+  paths = [
+    {
+      route: 'warehouses',
+      label: 'Bodegas',
+      icon: Warehouse,
+    },
+    {
+      route: 'locations',
+      label: 'Ubicaciones',
+      icon: MapPin,
+    },
+    {
+      route: 'people',
+      label: 'Personas',
+      icon: UserRound,
+    },
+  ];
+
+
+  logout(){
+    localStorage.setItem('isAuthenticated', 'false');
+    this.router.navigate(['/login']);
+  }
 
 }
