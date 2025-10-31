4fdbef6270b3c9ea34a61f810d50f9207a6ab010
Jchhm24 <jcchm2004@gmail.com>
2025-10-31 17:47:52 -0600

    Se añadio el service para poder proveer el usuario logeado en toda la app

---
+++ src/app/core/services/user-service.ts
@@ -0,0 +1,53 @@
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
+import { UserInterface } from '@shared/interfaces/user-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private platformId = inject(PLATFORM_ID);
+  private userSignal = signal<UserInterface | null>(null);
+  readonly user = this.userSignal.asReadonly();
+
+  constructor() {
+    this.loadUserFromStorage();
+  }
+
+  setUser(user: UserInterface | null): void {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      this.userSignal.set(user);
+    } else {
+      this.clearUser();
+    }
+  }
+
+  clearUser(): void {
+    localStorage.removeItem('user');
+    this.userSignal.set(null);
+  }
+
+  getUserRole(): 'admin' | 'user' {
+    return this.userSignal()?.role || 'user';
+  }
+
+  isAuthenticated(): boolean {
+    return this.userSignal() !== null;
+  }
+
+  private loadUserFromStorage(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      try {
+        const user: UserInterface = JSON.parse(userData);
+        this.userSignal.set(user);
+      } catch (error) {
+        console.error('Error parsing user data from localStorage', error);
+        this.clearUser();
+      }
+    }
+  }
+}
