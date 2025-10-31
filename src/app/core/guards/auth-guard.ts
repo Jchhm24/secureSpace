93bbb4fd654b1cd227783c98c164e3e951e6b49d
Jchhm24 <jcchm2004@gmail.com>
2025-10-31 17:50:37 -0600

    Se cambio el consumo la verificación el usuario logeado para saber si se puede proveer alguna intefaz de la app

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,13 +1,15 @@
 import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '@core/services/user-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const platformId = inject(PLATFORM_ID);
+  const userService = inject(UserService);
 
   if (isPlatformBrowser(platformId)) {
-    const isAuthenticated = localStorage.getItem('token') !== null;
+    const isAuthenticated = userService.isAuthenticated();
 
     if (!isAuthenticated) {
       router.navigate(['/login']);
