4073c3d8682c522d9738249d0762b0db3498ff3e
Jchhm24 <jcchm2004@gmail.com>
2025-11-01 22:41:00 -0600

    reemplazar datos simulados de bodegas con llamada a API para obtener información dinámica

--- src/app/core/services/warehouse-service.ts
+++ src/app/core/services/warehouse-service.ts
@@ -1,7 +1,10 @@
-import { Injectable, signal } from '@angular/core';
-import { warehouses } from '@features/warehouses/const/warehouses-data';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environment/environment';
 import { Warehouse } from '@features/warehouses/interfaces/warehouse-interface';
-import { of } from 'rxjs';
+import { catchError, of } from 'rxjs';
+import { UserService } from './user-service';
+import { WarehouseHomeData } from '@features/warehouses/interfaces/warehouse-home-data-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,20 +14,64 @@ export class WarehouseService {
     warehouses: new Map<string, Warehouse>(),
   });
 
+  private http = inject(HttpClient);
+  private apiUrl = environment.API_URL;
+  private user = inject(UserService);
+
+  private _totalWarehouses = signal<{
+    total: number;
+    available: number;
+    occupied: number;
+  }>({
+    total: 0,
+    available: 0,
+    occupied: 0,
+  });
+
+  readonly totalWarehouses = this._totalWarehouses.asReadonly();
+
   constructor() {
     this.getWarehouses();
   }
 
   getWarehouses() {
-    const mockWarehouses: Warehouse[] = warehouses;
-
-    of(mockWarehouses).subscribe((result) => {
-      result.forEach((warehouse) => {
-        this.state().warehouses.set(warehouse.id, warehouse);
+    const token = this.user.getToken();
+    this.http
+      .get<any>(`${this.apiUrl}/bodegasHome`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching total warehouses count:', error);
+          return of(null);
+        }),
+      )
+      .subscribe((response: WarehouseHomeData) => {
+        const {info, data } = response;
+        // cards total warehouses
+        this._totalWarehouses.set({
+          total: info.total || 0,
+          available: info.libres || 0,
+          occupied: info.ocupadas || 0,
+        });
+        // table warehouses dat
+        of(data).subscribe((result) => {
+          result.forEach((warehouses) => {
+            this.state().warehouses.set(warehouses.id, {
+              id: warehouses.id,
+              name: warehouses.nombre,
+              location: warehouses.ubicacion,
+              owner: warehouses.personaAsignada,
+              status: warehouses.estado === 'Disponible' ? true : false,
+              register: warehouses.ultimoRegistro,
+              idOwner: warehouses.IdpersonaAsignada,
+            });
+          });
+          this.state.set({ warehouses: this.state().warehouses });
+        });
       });
-
-      this.state.set({ warehouses: this.state().warehouses });
-    });
   }
 
   getFormattedWarehouses() {
