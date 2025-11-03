f72afc870f40531e933a07e0d499e34b2b2e8162
Jchhm24 <jcchm2004@gmail.com>
2025-11-02 18:28:56 -0600

    feat: agregar interfaces para la creación de almacenes

---
+++ src/app/features/warehouses/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './warehouse-create-interface';
+export * from './warehouse-home-data-interface';
+export * from './warehouse-interface';
---
+++ src/app/features/warehouses/interfaces/warehouse-create-interface.ts
@@ -0,0 +1,5 @@
+export interface WarehouseCreateInterface {
+  name: string;
+  locationId: string;
+  ownerId: string;
+}
