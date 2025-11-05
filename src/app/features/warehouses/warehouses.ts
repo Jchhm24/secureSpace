a5459d97a2f03de8a4a6a968ffcf7a8e2c08d4c4
Jchhm24 <jcchm2004@gmail.com>
2025-11-05 09:33:09 -0600

    feat: agregar modal para crear bodegas y mejorar la lógica de suscripción

--- src/app/features/warehouses/warehouses.ts
+++ src/app/features/warehouses/warehouses.ts
@@ -1,19 +1,24 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LayoutService } from '@core/services/layout-service';
 import { WarehouseCard } from '@shared/components/warehouse-card/warehouse-card';
 import { WarehousesTable } from './components/warehouses-table/warehouses-table';
 import { WarehouseService } from '@core/services/warehouse-service';
+import { Subscription } from 'rxjs';
+import { useToggle } from '@shared/hooks/use-toggle';
+import { CreateWarehouseModal } from "./components/create-warehouse-modal/create-warehouse-modal";
 
 @Component({
   selector: 'app-warehouses',
-  imports: [WarehouseCard, WarehousesTable],
+  imports: [WarehouseCard, WarehousesTable, CreateWarehouseModal],
   templateUrl: './warehouses.html',
   styleUrl: './warehouses.css',
 })
-export class Warehouses implements OnInit {
+export class Warehouses implements OnInit, OnDestroy {
   private layoutService = inject(LayoutService);
   protected readonly warehouseService = inject(WarehouseService);
-  protected totalWarehouses = this.warehouseService.totalWarehouses
+  protected totalWarehouses = this.warehouseService.totalWarehouses;
+  private clickSub?: Subscription;
+  protected modal = useToggle();
 
   ngOnInit(): void {
     this.layoutService.setConfig({
@@ -22,10 +27,15 @@ export class Warehouses implements OnInit {
       button: {
         label: 'Crear Bodega',
         ariaLabel: 'Crear Bodega',
-        functionAction: () => {
-          alert('Boton para crear bodega');
-        },
       },
     });
+
+    this.clickSub = this.layoutService.onButtonClick$.subscribe(() => {
+      this.modal.open();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.clickSub?.unsubscribe();
   }
 }
