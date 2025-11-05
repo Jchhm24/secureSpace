2de98c2983122ac966417606ce04789d31c550b8
Jchhm24 <jcchm2004@gmail.com>
2025-11-05 09:31:29 -0600

    feat: mejorar el servicio de ubicaciones con lógica para obtener y almacenar selecciones

--- src/app/core/services/locations-service.ts
+++ src/app/core/services/locations-service.ts
@@ -1,15 +1,30 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environment/environment';
 import { Locations } from '@features/locations/const/locations-data';
-import { Location } from '@features/locations/interfaces/locations-interface';
 import { of } from 'rxjs';
+import { UserService } from './user-service';
+import { selectLocations, Location } from '@features/locations/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocationsService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.API_URL;
+  private user = inject(UserService);
+
+  // TODO: Become this state to private when i work on the locations service more
   state = signal({
     locations: new Map<number, Location>(),
+    selectLocations: new Map<string, selectLocations>(),
   });
+
+  locations = computed(() => Array.from(this.state().locations.values()));
+
+  selectLocations = computed(() =>
+    Array.from(this.state().selectLocations.values()),
+  );
 
   constructor() {
     this.getLocations();
@@ -23,11 +38,35 @@ export class LocationsService {
         result.map((location) => [location.id, location]),
       );
 
-      this.state.set({ locations: newLocations });
+      this.state.update((state) => ({
+        ...state,
+        locations: newLocations,
+      }));
     });
   }
 
   getFormattedLocations() {
     return Array.from(this.state().locations.values());
+  }
+
+  getSelectLocations() {
+    const token = this.user.getToken();
+    this.http
+      .get<{ data: selectLocations[] }>(`${this.apiUrl}/locationsSelect`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .subscribe((result) => {
+        const newSelectLocations = new Map(
+          result.data.map((location) => [location.id, location]),
+        );
+
+        this.state.update((state) => ({
+          ...state,
+          selectLocations: newSelectLocations,
+        }));
+      });
   }
 }
