a4c97e0cbb6cf8e00a39fcf6b0fcfa07367c761d
Jchhm24 <jcchm2004@gmail.com>
2025-11-06 20:34:18 -0600

    feat: agregar interfaz para la selección de personas

--- src/app/features/people/interfaces/index.ts
+++ src/app/features/people/interfaces/index.ts
@@ -1,3 +1,3 @@
 export * from './people-data-interface';
 export * from './people-interface';
-
+export * from './select-people-interface';
---
+++ src/app/features/people/interfaces/select-people-interface.ts
@@ -0,0 +1,4 @@
+export interface selectPeople {
+  id: string;
+  nombre: string;
+}
