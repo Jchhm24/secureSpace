e3fad472507282b583d6645235c6afa7e718133f
Jchhm24 <jcchm2004@gmail.com>
2025-11-09 08:21:20 -0600

    feat: se añadio la sección de my-warehouses para los usuarios

---
+++ src/app/features/my-warehouses/my-warehouses.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-warehouses',
+  imports: [],
+  templateUrl: './my-warehouses.html',
+  styleUrl: './my-warehouses.css'
+})
+export class MyWarehouses {
+
+}
