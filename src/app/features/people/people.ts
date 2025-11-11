0753a7e654ec0d0def771f851cd1bca326ab5aba
Jchhm24 <jcchm2004@gmail.com>
2025-11-11 10:14:32 -0600

    se añadió el componente CreatePeopleModal y se actualizó la lógica para manejar la apertura del modal

--- src/app/features/people/people.ts
+++ src/app/features/people/people.ts
@@ -1,15 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PeopleTable } from './components/people-table/people-table';
 import { LayoutService } from '@core/services/layout-service';
+import { CreatePeopleModal } from './components/create-people-modal/create-people-modal';
+import { useToggle } from '@shared/hooks/use-toggle';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-people',
-  imports: [PeopleTable],
+  imports: [PeopleTable, CreatePeopleModal],
   templateUrl: './people.html',
   styleUrl: './people.css'
 })
 export class People implements OnInit {
-  layoutService = inject(LayoutService);
+  private layoutService = inject(LayoutService);
+  protected modal = useToggle();
+  private clickSub?: Subscription;
 
   ngOnInit(): void {
     this.layoutService.setConfig({
@@ -19,5 +24,13 @@ export class People implements OnInit {
         ariaLabel: 'Agregar Persona',
       }
     });
+
+    this.clickSub = this.layoutService.onButtonClick$.subscribe(() => {
+      this.modal.open();
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.clickSub?.unsubscribe();
   }
 }
