fb8dacd4e668208af8452d1a5230232e8f0cb769
Jchhm24 <jcchm2004@gmail.com>
2025-11-12 09:55:40 -0600

    se añadió el componente modal para crear nuevas ubicaciones y se actualizó la lógica para manejar su apertura

--- src/app/features/locations/locations.ts
+++ src/app/features/locations/locations.ts
@@ -1,15 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { LayoutService } from '@core/services/layout-service';
 import { LocationsTable } from './components/locations-table/locations-table';
+import { useToggle } from '@shared/hooks/use-toggle';
+import { Subscription } from 'rxjs';
+import { CreateLocationModal } from './components/create-location-modal/create-location-modal';
 
 @Component({
   selector: 'app-locations',
-  imports: [LocationsTable],
+  imports: [LocationsTable, CreateLocationModal],
   templateUrl: './locations.html',
-  styleUrl: './locations.css'
+  styleUrl: './locations.css',
 })
 export class Locations implements OnInit {
-  private layoutService = inject(LayoutService)
+  private layoutService = inject(LayoutService);
+  protected modal = useToggle();
+  private clickSub?: Subscription;
 
   ngOnInit(): void {
     this.layoutService.setConfig({
@@ -19,6 +24,14 @@ export class Locations implements OnInit {
         label: 'Agregar ubicación',
         ariaLabel: 'Agregar ubicación',
       },
-    })
+    });
+
+    this.clickSub = this.layoutService.onButtonClick$.subscribe(() => {
+      this.modal.open();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.clickSub?.unsubscribe();
   }
 }
