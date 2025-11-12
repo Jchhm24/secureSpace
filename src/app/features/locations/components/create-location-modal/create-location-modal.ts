9968eba236595afa5afa54bd89a6345dfbe2f098
Jchhm24 <jcchm2004@gmail.com>
2025-11-12 09:55:32 -0600

    se añadió el componente modal para crear nuevas ubicaciones, incluyendo su lógica y estilos

---
+++ src/app/features/locations/components/create-location-modal/create-location-modal.ts
@@ -0,0 +1,54 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IconService } from '@core/services/icon-service';
+import { LocationsService } from '@core/services/locations-service';
+import { ToastService } from '@core/services/toast-service';
+import { ButtonComponent } from '@shared/components/button-component/button-component';
+import { ButtonIcon } from '@shared/components/button-icon/button-icon';
+import { InputComponent } from '@shared/components/input-component/input-component';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-create-location-modal',
+  imports: [
+    LucideAngularModule,
+    InputComponent,
+    ButtonComponent,
+    ButtonIcon,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './create-location-modal.html',
+  styleUrl: './create-location-modal.css',
+})
+export class CreateLocationModal {
+  isOpen = input.required<boolean>();
+  closeClick = output<void>();
+
+  private locationsService = inject(LocationsService);
+  private toast = inject(ToastService);
+  protected icons = inject(IconService).icons;
+
+  protected locationControl = new FormGroup({
+    name: new FormControl(''),
+  });
+
+  closeModal(): void {
+    this.closeClick.emit();
+  }
+
+  onSubmit(): void {
+    this.locationControl.markAllAsTouched();
+    if(this.locationControl.valid){
+      this.locationsService.createLocation(this.locationControl.value.name!).subscribe({
+        next: (response) => {
+          if(response.success){
+            this.toast.show(response.message, 'success');
+            this.closeModal();
+          } else {
+            this.toast.show(response.message, 'error');
+          }
+        }
+      })
+    }
+  }
+}
