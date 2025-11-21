9b52b22e8dcfe80d5f69fea2aa9e65d52bb4ecaf
Jchhm24 <jcchm2004@gmail.com>
2025-11-21 10:49:11 -0600

    se mejoró la gestión de apertura de bodegas, incluyendo el manejo de mensajes de éxito y error

--- src/app/core/services/user-warehouses-service.ts
+++ src/app/core/services/user-warehouses-service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environment/environment';
 import { Warehouse } from '@features/warehouses/interfaces';
 import { Socket } from 'socket.io-client';
-import { catchError, of } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,7 @@ export class UserWarehousesService {
   private apiUrl = environment.API_URL;
   private wsUrl = environment.WS_URL;
   private socket: Socket | null = null;
+  private token = this.user.getToken();
 
   private state = signal({
     warehouses: new Map<string, Warehouse>(),
@@ -28,17 +29,16 @@ export class UserWarehousesService {
     return this.state().warehouses.size;
   });
 
-  constructor(){
+  constructor() {
     this.getWarehouseByUser(this.user.getUserId());
   }
 
   getWarehouseByUser(userId: string) {
-    const token = this.user.getToken();
-
+    if (!userId) return;
     this.http
       .get(`${this.apiUrl}/bodegasUsuario/${userId}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${this.token}`,
         },
       })
       .pipe(
@@ -56,6 +56,26 @@ export class UserWarehousesService {
         });
         this.state.set({ warehouses: warehouse });
       });
+  }
+
+  openWarehouse(id: string): Observable<{ success: boolean; message: string}> {
+    let message = '';
+    return this.http
+      .post(`${this.apiUrl}/bodega/requestApertura/${id}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      })
+      .pipe(
+        tap((response: any) => {
+          message = response?.message || 'Bodega abierta con éxito';
+        }),
+        map(() => ({ success: true, message: message })),
+        catchError((error) => {
+          console.error('Error opening warehouse:', error);
+          return of({ success: false, message: 'Error opening warehouse' });
+        }),
+      )
   }
 
   private mapApiToWarehouse(apiData: any): Warehouse {
