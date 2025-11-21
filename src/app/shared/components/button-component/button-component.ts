be5932a54a9cbc1c80601dedb7f0a0fb70d699c0
Jchhm24 <jcchm2004@gmail.com>
2025-11-21 09:57:23 -0600

    se añadió la gestión de escala en el componente Button para mejorar la interacción visual al hacer hover

--- src/app/shared/components/button-component/button-component.ts
+++ src/app/shared/components/button-component/button-component.ts
@@ -7,6 +7,7 @@ import { Component, input, output } from '@angular/core';
   styleUrl: './button-component.css',
   host: {
     '[style.--width]': 'width',
+    '[style.--scale-hover]': 'scaleHover',
   },
 })
 export class ButtonComponent {
@@ -16,6 +17,7 @@ export class ButtonComponent {
 
   width = 'auto';
   textAlign = 'start';
+  scaleHover = '1.05';
 
   onClick(): void {
     this.clickAction.emit();
